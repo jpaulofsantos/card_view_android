87f2b35727a7cc4c131ed1b82703871677812185
Joao Paulo Faustino Santos <jpaulofsantos@gmail.com>
2021-03-03 23:20:54 -0300

    concluding PostagemAdapter and generating cards

--- app/src/main/java/com/example/cardview/activity/MainActivity.java
+++ app/src/main/java/com/example/cardview/activity/MainActivity.java
@@ -8,10 +8,15 @@ import android.os.Bundle;
 
 import com.example.cardview.R;
 import com.example.cardview.adapter.PostagemAdapter;
+import com.example.cardview.model.Postagem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerPostagem;
+    private List<Postagem> postagens = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,22 @@ public class MainActivity extends AppCompatActivity {
         recyclerPostagem.setLayoutManager(layoutManager);
 
         //Definindo o Adapter
+        this.prepararPostagem();
 
-        PostagemAdapter postagemAdapter = new PostagemAdapter();
+        PostagemAdapter postagemAdapter = new PostagemAdapter(postagens);
 
         recyclerPostagem.setAdapter(postagemAdapter);
+    }
+
+    public void prepararPostagem() {
+        Postagem post1 = new Postagem("User1", "Viagem1", R.drawable.imagem1);
+        this.postagens.add(post1);
+        Postagem post2 = new Postagem("User2", "Viagem2", R.drawable.imagem2);
+        this.postagens.add(post2);
+        Postagem post3 = new Postagem("User3", "Viagem3", R.drawable.imagem3);
+        this.postagens.add(post3);
+        Postagem post4 = new Postagem("User4", "Viagem4", R.drawable.imagem4);
+        this.postagens.add(post4);
+
     }
 }
--- app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
+++ app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
@@ -1,43 +1,69 @@
 package com.example.cardview.adapter;
 
 import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cardview.R;
+import com.example.cardview.model.Postagem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> { //criando a classe. Após criar o MyViewHolder, inserir PostagemAdapter.MyViewHolder e implementar os metodos
+
+    private List<Postagem> listaPostagem;
+
+    public PostagemAdapter(List<Postagem> listPostagens) {
+        this.listaPostagem = listPostagens;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View itemLista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.postagem_detalhe, parent, false);
+
+        return new MyViewHolder(itemLista); //instanciando MyHolder passando  a view (itemLista)
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) { //holder é um objeto de MyViewHolder, desta forma tem acesso aos componentes
+
+        Postagem p = listaPostagem.get(position);
+
+        holder.textNome.setText(p.getNome());
+        holder.textPostagem.setText(p.getPostagem());
+        holder.imagePostagem.setImageResource(p.getImagem());
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaPostagem.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder { //criando MyViewHolder
 
-        private TextView nome;
-        private TextView postagem;
-        private ImageView imagem;
+        private TextView textNome;
+        private TextView textPostagem;
+        private ImageView imagePostagem;
 
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            textNome = itemView.findViewById(R.id.textNome);
+            textPostagem = itemView.findViewById(R.id.textPostagem);
+            imagePostagem = itemView.findViewById(R.id.imagePostagem);
         }
     }
-
-
 }
